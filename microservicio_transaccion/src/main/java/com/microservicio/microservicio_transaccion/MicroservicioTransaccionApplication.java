e17c35601ff7f5ffc98cadb3cb9229e384fdfe8b
julian98789 <julian_gomez69222@elpoli.edu.co>
2024-09-16 16:40:45 -0500

    Agregue dependencias de spring boot

---
+++ microservicio_transaccion/src/main/java/com/microservicio/microservicio_transaccion/MicroservicioTransaccionApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.microservicio_transaccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioTransaccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioTransaccionApplication.class, args);
+	}
+
+}
